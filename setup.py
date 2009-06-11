74c555da0e0c1b5000ffd33f77415813f228145a
Gary Martin <gary@garycmartin.com>
2009-06-11 05:08:20 +0100

    Quick clean up of the source repo.

--- setup.py
+++ setup.py
@@ -5,9 +5,4 @@ try:
     from sugar.activity import bundlebuilder
     bundlebuilder.start()
 except ImportError:
-    import os
-    os.system("find ./ | sed 's,^./,Clock/,g' > MANIFEST")
-    os.chdir("..")
-    os.system("zip -r Clock.xo Clock")
-    os.system("mv Clock.xo ./Clock")
-    os.chdir("Clock")
+    print "Error: sugar.activity.Bundlebuilder not found."
